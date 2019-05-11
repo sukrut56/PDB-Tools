770cdaf21941c2dcec2dc27cde8268309cab8ea2
Sukrut <37614346+sukrut56@users.noreply.github.com>
2019-05-11 07:27:14 +0530

    Update CHAIN SELECTION.py

--- PDB Tools/CHAIN SELECTION.py
+++ PDB Tools/CHAIN SELECTION.py
@@ -1,11 +1,8 @@
 import os
-import pandas as pd
 
-with open ("F:/FINAL PROJECT/GROMIHA DATASET/PDB FILES/HIGH/1atn.pdb") as data:
+with open ("F:/.pdb") as data:
 	p= list(data)
 
-n= []
-l=[]
 t=[]
 for t in p: 
 	k= t.split()
